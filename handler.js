c6484b2eccc4083b144b43ee97eacd87c0a95270
maximeroma <m.roma@techexmachina.com>
2019-11-17 20:23:36 +0100

    add retry behaviour

--- handler.js
+++ handler.js
@@ -2,6 +2,17 @@ module.exports.add = async ({ x, y }) => {
     return x + y
 }
 
+class NumberIsTooBig extends Error {
+    constructor(n) {
+        super(`${n} is too big`)
+        this.name = 'NumberIsTooBig'
+        Error.captureStackTrace(this, NumberIsTooBig)
+    }
+}
+
 module.exports.double = async sum => {
+    if (sum > 50) {
+        throw new NumberIsTooBig(sum)
+    }
     return sum * 2
 }
